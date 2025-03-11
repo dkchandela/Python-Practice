10c7fb434a9cf47a2f180577eae9cf0bd92af5e6
dkchandela <dkchanija16398@gmail.com>
2025-03-11 11:15:08 +0530

    Code Updated

--- loops.py
+++ loops.py
@@ -2,3 +2,22 @@ count=1
 while count<=5:
     print("Hy")
     count +=1
+sq=[]
+a=1
+while a<=10:
+    sq.append(a*a)
+    a+=1
+print(sq)
+# i=0
+# while i<len(sq):
+#     if sq[i]==int(input("number is:")):
+#         print("found number at index", i)
+#     i+=1
+
+n = int(input("Number is: "))
+sum =0
+i=1
+while i<=n:
+    sum=sum+i
+    i=i+1
+print(sum)
